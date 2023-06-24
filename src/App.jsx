cfcb8d53922497ffcb12f2945eb403c4ce5d9c78
Joel Onwuanaku <joelebuka@gmail.com>
2023-06-24 21:02:57 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+const App = () => {
+  return (
+    <section className="flex flex-col h-[100vh] w-[100vw] justify-center items-center">
+      <h1 className="text-xl font-bold">Hello World</h1>
+      <p>A SCSS, TailwindCSS, Alpine, React and Vite Boilerplate</p>
+    </section>
+  );
+};
+
+export default App;
